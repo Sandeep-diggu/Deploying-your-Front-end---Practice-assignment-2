073e6637b754ae38789413f8bc2753f48641b07e
Sandeep-diggu <sandeepkumar684@gmail.com>
2025-04-02 14:31:14 +0530

    sandy

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Sandeep kumar</h1>
+      <h3>phone no : 1234567890</h3>
+    </>
+  )
+}
+
+export default App
